f47f014b57ac03c62988a3b33e1fd09d2b146532
ArjunaAcchaDipa <arjunaacchadipa@gmail.com>
2024-01-23 00:32:38 +0700

    Add feature to get HistoryBalance by User and combine updateBalance with addBalanceHistory

--- src/main/java/com/btengine/btlink/repository/BalanceHistoryRepository.java
+++ src/main/java/com/btengine/btlink/repository/BalanceHistoryRepository.java
@@ -2,6 +2,13 @@ package com.btengine.btlink.repository;
 
 import com.btengine.btlink.model.BalanceHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface BalanceHistoryRepository extends JpaRepository<BalanceHistory, String> {
+    @Query("SELECT bh FROM BalanceHistory bh WHERE bh.balance.skBalance = :fkBalance")
+    List<BalanceHistory> findBalanceHistoryByFkBalance(@Param("fkBalance") UUID fkBalance);
 }
--- src/main/java/com/btengine/btlink/service/BalanceHistoryService.java
+++ src/main/java/com/btengine/btlink/service/BalanceHistoryService.java
@@ -1,16 +1,23 @@
 package com.btengine.btlink.service;
 
+import com.btengine.btlink.model.Balance;
 import com.btengine.btlink.model.BalanceHistory;
 import com.btengine.btlink.repository.BalanceHistoryRepository;
+import com.btengine.btlink.repository.BalanceRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BalanceHistoryService {
+    @Autowired
+    BalanceRepository balanceRepository;
+
     @Autowired
     BalanceHistoryRepository balanceHistoryRepository;
 
@@ -25,8 +32,34 @@ public class BalanceHistoryService {
     }
 
     @Transactional
-    public void addBalanceHistory(BigDecimal initialBalance, BigDecimal finalBalance, String val) {
-        // add data to balance history
+    public BigDecimal addBalanceHistory(BigDecimal initialBalance, String val, UUID fkBalance) {
+        BalanceHistory balanceHistory = new BalanceHistory();
+
+        balanceHistory.setInitialBalance(initialBalance);
+
+        Boolean isBelowZero = Integer.parseInt(val) < 0;
+        BigDecimal value = new BigDecimal(val.substring(1));
+        BigDecimal finalBalance;
+        if (isBelowZero) {
+            finalBalance = initialBalance.subtract(value);
+        } else {
+            finalBalance = initialBalance.add(value);
+        }
+        balanceHistory.setFinalBalance(finalBalance);
+
+        balanceHistory.setVal(val);
+        balanceHistory.setCreatedAt(LocalDateTime.now());
+
+        Balance balance = balanceRepository.findBalanceHistoryByBalanceId(fkBalance);
+        balanceHistory.setBalance(balance);
+
+        balanceHistoryRepository.save(balanceHistory);
+
+        return(finalBalance);
+    }
+
+    public List<BalanceHistory> findBalanceHistoryByFkBalance(UUID fkBalance) {
+        return balanceHistoryRepository.findBalanceHistoryByFkBalance(fkBalance);
     }
 
 }
